4ce07d9af14efbed8d203fb7f9a1f1489385b700
Daniel Hopler <hopler.daniel@gmail.com>
2017-12-18 07:47:54 -0500

    Create RoomList & install Firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAC4Bj7lqqk3KRIzJf1S2oiceaIskuRB5g",
+    authDomain: "bloc-chat-5974e.firebaseapp.com",
+    databaseURL: "https://bloc-chat-5974e.firebaseio.com",
+    projectId: "bloc-chat-5974e",
+    storageBucket: "bloc-chat-5974e.appspot.com",
+    messagingSenderId: "172718334226"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+
+  render () {
+    return (
+
+    );
+  }
+}
+
+
+export default RoomList;
